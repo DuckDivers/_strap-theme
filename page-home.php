863169c803a8ef6af560e2b62a434f82973495f2
Howard <howarde@scubaboard.com>
2018-02-14 14:40:45 -0500

    Add header();
    adding get_header to homepage and integrating

--- page-home.php
+++ page-home.php
@@ -2,31 +2,9 @@
 /**
 * Template Name: Home Page
 */
-?>
-<!DOCTYPE html>
-<html <?php language_attributes(); ?>>
-<head>
-<meta charset="<?php bloginfo( 'charset' ); ?>">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="profile" href="http://gmpg.org/xfn/11">
-<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
-<!--[if lt IE 9]>
-<script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
-<script src="//cdn.jsdelivr.net/respond/1.4.2/respond.min.js"></script>
-<![endif]-->
+get_header();
 
-<?php wp_head(); ?>
-</head>
-
-<body <?php body_class(); ?>>
-<div id="page" class="site container-fluid"><div class="row">
-            <header id="masthead" class="site-header" role="banner">
-				<?php get_template_part( 'page-sections/header', 'section' ); ?>
-			</header><!-- #masthead -->
-
-<div id="content-home" class="site-content">
-
-<?php get_template_part( 'template-parts/content', 'slider' ); ?>
+get_template_part( 'template-parts/content', 'slider' ); ?>
 
 
 	<div id="full-width-container" class="content-area">
