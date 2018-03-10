9251a88aba40ade55568b160a32dfd3bf05f63bc
Howard <howarde@scubaboard.com>
2018-03-10 17:48:20 -0500

    Update page-faq.php

--- page-faq.php
+++ page-faq.php
@@ -2,21 +2,17 @@
 /**
 * Template Name: FAQ Page
 */
-
-
 get_header(); ?>
 
-	<div id="full-width-container" class="content-area">
-		<main id="main" class="site-main container" role="main">
+<main id="main" class="site-main col-12" role="main">
 
-			<?php while ( have_posts() ) : the_post(); ?>
+	<?php while ( have_posts() ) : the_post(); ?>
 
-				<?php get_template_part( 'template-parts/content', 'faq' ); ?>
+		<?php get_template_part( 'template-parts/content', 'faq' ); ?>
 
 
-			<?php endwhile; // End of the loop. ?>
+	<?php endwhile; // End of the loop. ?>
 
-		</main><!-- #main -->
-	</div>
+</main><!-- #main -->
 
 <?php get_footer(); ?>
