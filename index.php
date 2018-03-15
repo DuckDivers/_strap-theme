bccb1850436f970718b9a1b5445a738880316798
Howard <howarde@scubaboard.com>
2018-03-15 06:27:35 -0400

    Add Theme Mods

--- index.php
+++ index.php
@@ -12,12 +12,16 @@
  * @package Duck Diver Custom
  */
 
+$featured_size = (get_theme_mod('dd_featured_blog_image')) ? get_theme_mod('dd_featured_blog_image') : 'large' ;
+$featured_class = ($featured_size === 'large') ? 'col-12' : 'col-md-4';
+$post_class = ($featured_size === 'large') ? 'col-12' : 'col-md-8';
+
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
-			<?php if (get_theme_mod('blog_title_h1')) : ?>
-				<h1><?php echo get_theme_mod('blog_title_h1');?></h1>
+	<div id="primary-content" class="row">
+		<main id="main" class="site-main col-md-9" role="main">
+			<?php if (get_theme_mod('dd_blog_title_h1')) : ?>
+			 <h1><?php echo get_theme_mod('blog_title_h1');?></h1>
 			<?php else :?>
 				<h1><?php echo get_bloginfo() . ' Blog';?></h1>
 			<?php endif; ?>
@@ -33,44 +37,57 @@ get_header(); ?>
 			<?php /* Start the Loop */ ?>
 			<?php while ( have_posts() ) : the_post(); ?>
 
-			<article id="post-<?php the_ID(); ?>" <?php post_class( 'post__holder'); ?>>		<?php if(!is_singular()) : ?>
-				<header class="post-header">
-					<?php if(is_sticky()) : ?>
-						<h5 class="post-label"><?php _e("featured");?></h5>
-					<?php endif; ?>
-					<h2 class="post-title"><a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></h2>
-				</header>
-				<?php endif; 
+			<article id="post-<?php the_ID(); ?>" <?php post_class( 'post__holder'); ?>>		
+                <?php if(!is_singular()) : ?>
+				 <div class="row">
+                      <div class="col-12">
+                           <header class="post-header">
+                                <?php if(is_sticky()) : ?>
+                                <h5 class="post-label"><?php _e("featured");?></h5>
+                                <?php endif; ?>
+                                <h2 class="post-title"><a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></h2>
+                           </header>
+                      </div>
+		         </div>
+                 <div class="row">
+                      <div class="<?php echo $featured_class;?>">
+                           <?php endif; 
 					if (has_post_thumbnail()){
 						echo '<figure class="featured-thumbnail thumbnail">'.get_the_post_thumbnail().'</figure>';
-					}
-				?>
-				
-				<?php if ( !is_singular() ) : ?>
-				<!-- Post Content -->
-				<div class="post_content">
-					<div class="excerpt">
-							<?php
-
+					   }
+                        else $post_class = 'col-12';
+				    ?>
+                      </div>
+                      
+                      <?php if ( !is_singular() ) : ?>
+                      <!-- Post Content -->
+                      <div class="<?php echo $post_class;?>">
+                           <div class="post_content">
+                                <div class="excerpt">
+                                     <?php
+                            
+                        $excerpt_length = (get_theme_mod('dd_blog_excerpt_length')) ? get_theme_mod('dd_blog_excerpt_length') : 55 ;
+                        
 							if (has_excerpt()) {
 								the_excerpt();
 							} else {
 								$theContent = get_the_content();
 								$theContent = strip_shortcodes($theContent);
-								echo wp_trim_words( $theContent, 55, '...' );
+								echo wp_trim_words( $theContent, $excerpt_length, '...' );
 							} ?>
-						</div>
-					<?php $button_text = 'Read More' ;?>
-					<a href="<?php the_permalink() ?>" class="btn btn-primary"><?php echo $button_text; ?></a>
-					<div class="clear"></div>
-				</div>
-
-				<?php else :?>
-				<!-- Post Content -->
-				<div class="post_content">
-					<?php the_content(''); ?>
-					<div class="clear"></div>
-				</div>
+                                   </div>
+                                <?php $button_text = (get_theme_mod('dd_read_more_text')) ? get_theme_mod('dd_read_more_text') : 'Read More' ;?>
+                                <a href="<?php the_permalink() ?>" class="btn btn-primary"><?php echo $button_text; ?></a>
+                                <div class="clear"></div>
+                           </div>
+                      </div>
+                    <?php else :?>
+                      <!-- Post Content -->
+                      <div class="post_content">
+                           <?php the_content(''); ?>
+                           <div class="clear"></div>
+                      </div>
+                 </div>
 				<!-- //Post Content -->
 				<?php endif; ?>
 
@@ -88,7 +105,8 @@ get_header(); ?>
 				<?php understrap_pagination(); ?>
 
 		</main><!-- #main -->
+        <aside class="col-md-3">
+            <?php get_sidebar(); ?>
+        </aside>
 	</div><!-- #primary -->
-
-<?php get_sidebar(); ?>
 <?php get_footer(); ?>
