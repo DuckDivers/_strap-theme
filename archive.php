b8059d584aac1f4ec6eb2c77900270a2eb1ebc82
Howard <howarde@scubaboard.com>
2018-03-15 06:38:06 -0400

    Add Theme Mods

--- archive.php
+++ archive.php
@@ -6,45 +6,95 @@
  *
  * @package Duck Diver Custom
  */
-
+$featured_size = (get_theme_mod('dd_featured_blog_image')) ? get_theme_mod('dd_featured_blog_image') : 'large' ;
+$featured_class = ($featured_size === 'large') ? 'col-12' : 'col-md-4';
+$post_class = ($featured_size === 'large') ? 'col-12' : 'col-md-8';
 get_header(); ?>
 
-	<main id="main" class="site-main col-md-9" role="main">
-
-		<?php if ( have_posts() ) : ?>
-
-			<header class="page-header">
-				<?php
+<div class="row">
+     <main id="main" class="site-main col-md-9" role="main">
+          
+          <?php if ( have_posts() ) : ?>
+          
+          <header class="page-header">
+               <?php
 					the_archive_title( '<h1 class="page-title">', '</h1>' );
 					the_archive_description( '<div class="taxonomy-description">', '</div>' );
 				?>
-			</header><!-- .page-header -->
+             </header><!-- .page-header -->
+          
+          <?php /* Start the Loop */ ?>
+          <?php while ( have_posts() ) : the_post(); ?>
+          
+          <article id="post-<?php the_ID(); ?>" <?php post_class( 'post__holder'); ?>>		
+                <?php if(!is_singular()) : ?>
+				 <div class="row">
+                      <div class="col-12">
+                           <header class="post-header">
+                                <?php if(is_sticky()) : ?>
+                                <h5 class="post-label"><?php _e("featured");?></h5>
+                                <?php endif; ?>
+                                <h2 class="post-title"><a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></h2>
+                           </header>
+                      </div>
+		         </div>
+                 <div class="row">
+                      <div class="<?php echo $featured_class;?>">
+                           <?php endif; 
+					if (has_post_thumbnail()){
+						echo '<figure class="featured-thumbnail thumbnail">'.get_the_post_thumbnail().'</figure>';
+					   }
+                        else $post_class = 'col-12';
+				    ?>
+                      </div>
+                      
+                      <?php if ( !is_singular() ) : ?>
+                      <!-- Post Content -->
+                      <div class="<?php echo $post_class;?>">
+                           <div class="post_content">
+                                <div class="excerpt">
+                                     <?php
+                            
+                        $excerpt_length = (get_theme_mod('dd_blog_excerpt_length')) ? get_theme_mod('dd_blog_excerpt_length') : 55 ;
+                        
+							if (has_excerpt()) {
+								the_excerpt();
+							} else {
+								$theContent = get_the_content();
+								$theContent = strip_shortcodes($theContent);
+								echo wp_trim_words( $theContent, $excerpt_length, '...' );
+							} ?>
+                                   </div>
+                                <?php $button_text = (get_theme_mod('dd_read_more_text')) ? get_theme_mod('dd_read_more_text') : 'Read More' ;?>
+                                <a href="<?php the_permalink() ?>" class="btn btn-primary"><?php echo $button_text; ?></a>
+                                <div class="clear"></div>
+                           </div>
+                      </div>
+                    <?php else :?>
+                      <!-- Post Content -->
+                      <div class="post_content">
+                           <?php the_content(''); ?>
+                           <div class="clear"></div>
+                      </div>
+                 </div>
+				<!-- //Post Content -->
+				<?php endif; ?>
 
-			<?php /* Start the Loop */ ?>
-			<?php while ( have_posts() ) : the_post(); ?>
-
-				<?php
-
-					/*
-					 * Include the Post-Format-specific template for the content.
-					 * If you want to override this in a child theme, then include a file
-					 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-					 */
-					get_template_part( 'template-parts/content', get_post_format() );
-				?>
-
-			<?php endwhile; ?>
-
-			<?php the_posts_navigation(); ?>
-
-		<?php else : ?>
-
-			<?php get_template_part( 'template-parts/content', 'none' ); ?>
-
-		<?php endif; ?>
-
-		</main><!-- #main -->
-        <aside class="col-md-3" id="sidebar">
-            <?php get_sidebar(); ?>
-        </aside>    
+			</article>          
+          <?php endwhile; ?>
+          
+          <?php the_posts_navigation(); ?>
+          
+          <?php else : ?>
+          
+          <?php get_template_part( 'template-parts/content', 'none' ); ?>
+          
+          <?php endif; ?>
+          
+          </main>
+     <!-- #main -->
+     <aside class="col-md-3" id="sidebar">
+          <?php get_sidebar(); ?>
+     </aside>    
+</div>
 <?php get_footer(); ?>
